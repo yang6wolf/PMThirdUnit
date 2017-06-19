5bf244728d1b7d0fec00d32d01341724577fad8b
杨晓鹏 <bjyangxiaopeng1@corp.netease.com>
2017-06-19 18:10:19 +0800

    编译脚本中,增加批量拷贝头文件的代码

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 rm Frameworks/PMThirdUnit.framework/PMThirdUnit
 
+
+#rm Frameworks/Headers/*.h
+#find Pods -name *.h |xargs -I{} cp {} Frameworks/PMThirdUnit.framework/Headers/
+
 xcodebuild -workspace PMThirdUnit.xcworkspace -scheme "PMThirdUnit" -configuration Release ARCHS='i386 x86_64' VALID_ARCHS='i386 x86_64' -sdk iphonesimulator10.3
 mv Frameworks/PMThirdUnit.framework/PMThirdUnit Frameworks/autobuild-simulator
 xcodebuild -workspace PMThirdUnit.xcworkspace -scheme "PMThirdUnit" -configuration Release ARCHS='armv7 arm64' VALID_ARCHS='armv7 arm64' -sdk iphoneos10.3
@@ -10,3 +14,4 @@ mv Frameworks/PMThirdUnit Frameworks/PMThirdUnit.framework/
 
 rm Frameworks/autobuild-*
 rm -rf Frameworks/PMThirdUnit.framework.dSYM/
+
