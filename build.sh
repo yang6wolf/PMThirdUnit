1f4b61e497c3a343b81228535520de97a2524ca2
杨晓鹏 <bjyangxiaopeng1@corp.netease.com>
2017-06-30 19:58:42 +0800

    [4.0.4] 删除Info.plist中的模拟器标识.编译脚本中增加删除plist的操作

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ rm Frameworks/PMThirdUnit.framework/PMThirdUnit
 
 xcodebuild -workspace PMThirdUnit.xcworkspace -scheme "PMThirdUnit" -configuration Release ARCHS='i386 x86_64' VALID_ARCHS='i386 x86_64' -sdk iphonesimulator10.3
 mv Frameworks/PMThirdUnit.framework/PMThirdUnit Frameworks/autobuild-simulator
+
+find Frameworks/ -name *.plist |xargs rm -rf
+
 xcodebuild -workspace PMThirdUnit.xcworkspace -scheme "PMThirdUnit" -configuration Release ARCHS='armv7 arm64' VALID_ARCHS='armv7 arm64' -sdk iphoneos10.3
 mv Frameworks/PMThirdUnit.framework/PMThirdUnit Frameworks/autobuild-iphone
 
