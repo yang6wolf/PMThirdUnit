cb0fd04a6aa7449aba2af74856f54295c09e700c
杨晓鹏 <bjyangxiaopeng1@corp.netease.com>
2017-10-30 11:15:28 +0800

    [4.5.2] 补充ProtocolBuffer的头文件暴露

--- Frameworks/PMThirdUnit.framework/Headers/GeneratedMessageBuilder.h
+++ Frameworks/PMThirdUnit.framework/Headers/GeneratedMessageBuilder.h
@@ -15,28 +15,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import <Foundation/Foundation.h>
-#import "Bootstrap.h"
-#import "AbstractMessage.h"
 #import "AbstractMessageBuilder.h"
-#import "CodedInputStream.h"
-#import "CodedOutputStream.h"
-#import "ConcreteExtensionField.h"
-#import "ExtendableMessage.h"
-#import "ExtendableMessageBuilder.h"
-#import "ExtensionField.h"
-#import "ExtensionRegistry.h"
-#import "Field.h"
-#import "GeneratedMessage.h"
-#import "GeneratedMessageBuilder.h"
-#import "Message.h"
-#import "MessageBuilder.h"
-#import "MutableExtensionRegistry.h"
-#import "MutableField.h"
-#import "PBArray.h"
-#import "UnknownFieldSet.h"
-#import "UnknownFieldSetBuilder.h"
-#import "Utilities.h"
-#import "WireFormat.h"
-//#import "Descriptor.pb.h"
-//#import "ObjectivecDescriptor.pb.h"
+
+@class PBUnknownFieldSetBuilder;
+
+@interface PBGeneratedMessageBuilder : PBAbstractMessageBuilder {
+}
+
+/* @protected */
+- (BOOL) parseUnknownField:(PBCodedInputStream*) input
+             unknownFields:(PBUnknownFieldSetBuilder*) unknownFields
+         extensionRegistry:(PBExtensionRegistry*) extensionRegistry
+                       tag:(SInt32) tag;
+
+- (void) checkInitialized;
+
+@end
--- Frameworks/PMThirdUnit.framework/Headers/MutableExtensionRegistry.h
+++ Frameworks/PMThirdUnit.framework/Headers/MutableExtensionRegistry.h
@@ -15,28 +15,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import <Foundation/Foundation.h>
-#import "Bootstrap.h"
-#import "AbstractMessage.h"
-#import "AbstractMessageBuilder.h"
-#import "CodedInputStream.h"
-#import "CodedOutputStream.h"
-#import "ConcreteExtensionField.h"
-#import "ExtendableMessage.h"
-#import "ExtendableMessageBuilder.h"
-#import "ExtensionField.h"
 #import "ExtensionRegistry.h"
-#import "Field.h"
-#import "GeneratedMessage.h"
-#import "GeneratedMessageBuilder.h"
-#import "Message.h"
-#import "MessageBuilder.h"
-#import "MutableExtensionRegistry.h"
-#import "MutableField.h"
-#import "PBArray.h"
-#import "UnknownFieldSet.h"
-#import "UnknownFieldSetBuilder.h"
-#import "Utilities.h"
-#import "WireFormat.h"
-//#import "Descriptor.pb.h"
-//#import "ObjectivecDescriptor.pb.h"
+
+@interface PBMutableExtensionRegistry : PBExtensionRegistry {
+@private
+  NSMutableDictionary* mutableClassMap;
+}
+
++ (PBMutableExtensionRegistry*) registry;
+
+- (void) addExtension:(id<PBExtensionField>) extension;
+
+@end
